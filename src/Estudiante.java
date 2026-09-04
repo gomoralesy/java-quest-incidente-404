47c5b35ed7606a3588436f1697f590c79ff805df
gomoralesy <go.moralesy@duocuc.cl>
2026-09-03 21:10:40 -0400

    JAVA-003 - Agregar constructor de Estudiante

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -3,4 +3,10 @@ public class Estudiante {
     private String nombre;
     private String carrera;
     private int edad;
+
+    public Estudiante(String nombre, int edad, String carrera) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.carrera = carrera;
+    }
 }
